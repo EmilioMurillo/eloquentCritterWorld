78756f4a3db79acf4fe8b4ff316153a8da89f6be
resilientcoders6 <emilio.a.murillo@gmail.com>
2017-10-09 22:49:39 -0400

    action types not working, comenting out action type methods for now

--- js/eloquent.js
+++ js/eloquent.js
@@ -187,75 +187,75 @@ for (var i = 0; i < 5; i++) {
 };
 //animateWorld(world);
 
-// Action types (& animate world) are not defined??? Which is why I cannot animate(world) or later add new life forms or a new world 
+// Action types (& animate world) are not defined??? Which is why I cannot animate(world) or later add new life forms or a new world
 
-function dirPlus(dir, n) {
-  var index = directionNames.indexOf(dir);
-  return directionNames[(index + n + 8) % 8];
-}
-
-function WallFollower() {
-  this.dir = "s";
-}
-
-WallFollower.prototype.act = function(view) {
-  var start = this.dir;
-  if (view.look(dirPlus(this.dir, -3)) != " ")
-    start = this.dir = dirPlus(this.dir, -2);
-  while (view.look(this.dir) != " ") {
-    this.dir = dirPlus(this.dir, 1);
-    if (this.dir == start) break;
-  }
-  return {type: "move", direction: this.dir};
-};
-
-actionTypes.grow = function(critter) {
-  critter.energy += 0.5;
-  return true;
-};
-actionTypes.move = function(critter, vector, action) {
-  var dest = this.checkDestination(action, vector);
-  if (dest == null ||
-      critter.energy <= 1 ||
-      this.grid.get(dest) != null)
-    return false;
-  critter.energy -= 1;
-  this.grid.set(vector, null);
-  this.grid.set(dest, critter);
-  return true;
-};
-actionTypes.eat = function(critter, vector, action) {
-  var dest = this.checkDestination(action, vector);
-  var atDest = dest != null && this.grid.get(dest);
-  if (!atDest || atDest.energy == null)
-    return false;
-  critter.energy += atDest.energy;
-  this.grid.set(dest, null);
-  return true;
-};
-actionTypes.reproduce = function(critter, vector, action) {
-  var baby = elementFromChar(this.legend,
-                             critter.originChar);
-  var dest = this.checkDestination(action, vector);
-  if (dest == null ||
-      critter.energy <= 2 * baby.energy ||
-      this.grid.get(dest) != null)
-    return false;
-  critter.energy -= 2 * baby.energy;
-  this.grid.set(dest, baby);
-  return true;
-};
-
-
-function Plant() {
-  this.energy = 3 + Math.random() * 4;
-}
-Plant.prototype.act = function(view) {
-  if (this.energy > 15) {
-    var space = view.find(" ");
-    if (space)
-      return {type: "reproduce", direction: space};
-  }
-  if (this.energy < 20)
-    return {type: "grow"};
-};
+// function dirPlus(dir, n) {
+//   var index = directionNames.indexOf(dir);
+//   return directionNames[(index + n + 8) % 8];
+// }
+//
+// function WallFollower() {
+//   this.dir = "s";
+// }
+//
+// WallFollower.prototype.act = function(view) {
+//   var start = this.dir;
+//   if (view.look(dirPlus(this.dir, -3)) != " ")
+//     start = this.dir = dirPlus(this.dir, -2);
+//   while (view.look(this.dir) != " ") {
+//     this.dir = dirPlus(this.dir, 1);
+//     if (this.dir == start) break;
+//   }
+//   return {type: "move", direction: this.dir};
+// };
+//
+// actionTypes.grow = function(critter) {
+//   critter.energy += 0.5;
+//   return true;
+// };
+// actionTypes.move = function(critter, vector, action) {
+//   var dest = this.checkDestination(action, vector);
+//   if (dest == null ||
+//       critter.energy <= 1 ||
+//       this.grid.get(dest) != null)
+//     return false;
+//   critter.energy -= 1;
+//   this.grid.set(vector, null);
+//   this.grid.set(dest, critter);
+//   return true;
+// };
+// actionTypes.eat = function(critter, vector, action) {
+//   var dest = this.checkDestination(action, vector);
+//   var atDest = dest != null && this.grid.get(dest);
+//   if (!atDest || atDest.energy == null)
+//     return false;
+//   critter.energy += atDest.energy;
+//   this.grid.set(dest, null);
+//   return true;
+// };
+// actionTypes.reproduce = function(critter, vector, action) {
+//   var baby = elementFromChar(this.legend,
+//                              critter.originChar);
+//   var dest = this.checkDestination(action, vector);
+//   if (dest == null ||
+//       critter.energy <= 2 * baby.energy ||
+//       this.grid.get(dest) != null)
+//     return false;
+//   critter.energy -= 2 * baby.energy;
+//   this.grid.set(dest, baby);
+//   return true;
+// };
+//
+//
+// function Plant() {
+//   this.energy = 3 + Math.random() * 4;
+// }
+// Plant.prototype.act = function(view) {
+//   if (this.energy > 15) {
+//     var space = view.find(" ");
+//     if (space)
+//       return {type: "reproduce", direction: space};
+//   }
+//   if (this.energy < 20)
+//     return {type: "grow"};
+// };
